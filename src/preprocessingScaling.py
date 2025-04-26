847345f54756d6b8f2ea57df6eeaf6ebd4242851
avdhootgautam <avdhootgautam416@gmail.com>
2025-04-26 23:46:43 +0530

    change in a scaling feature

--- src/preprocessingScaling.py
+++ src/preprocessingScaling.py
@@ -8,9 +8,13 @@ def preprocessing_scaling(df,set_of_standard_scaling_column,set_of_min_max_scali
         min_max_scaler=MinMaxScaler()
         robust_scaler=RobustScaler()
         max_abs_scaler=MaxAbsScaler()
-        df[list(set_of_standard_scaling_column)]=standard_scaler.fit_transform(df[list(set_of_standard_scaling_column)])
-        df[list(set_of_min_max_scaling_column)]=standard_scaler.fit_transform(df[list(set_of_min_max_scaling_column)])
-        df[list(set_of_robust_scaling_columns)]=standard_scaler.fit_transform(df[list(set_of_robust_scaling_columns)])
-        df[list(set_of_max_absolute_scaling_columns)]=standard_scaler.fit_transform(df[list(set_of_max_absolute_scaling_columns)])
-        
+        if set_of_standard_scaling_column:
+            df[list(set_of_standard_scaling_column)]=standard_scaler.fit_transform(df[list(set_of_standard_scaling_column)])
+        if set_of_min_max_scaling_column:
+            df[list(set_of_min_max_scaling_column)]=standard_scaler.fit_transform(df[list(set_of_min_max_scaling_column)])
+        if set_of_robust_scaling_columns:
+            df[list(set_of_robust_scaling_columns)]=standard_scaler.fit_transform(df[list(set_of_robust_scaling_columns)])
+        if set_of_max_absolute_scaling_columns:
+            df[list(set_of_max_absolute_scaling_columns)]=standard_scaler.fit_transform(df[list(set_of_max_absolute_scaling_columns)])
+
         return df
